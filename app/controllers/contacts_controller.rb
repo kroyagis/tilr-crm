ed7d4ae6ca1a1d2e70716747f9fba100070399e1
Ji You <kroyagis@gmail.com>
2016-11-07 14:41:03 -0500

    —added CRUD methods within contacts_controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,42 @@
 class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to(contacts_url), notice: 'The contact has been created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    if @contact.update_attributes(contact_params)
+      flash[:notice] = 'The contact has been updated.'
+      redirect_to contact_path
+    else
+      flash[:notice] = 'The contact was not updated successfully.'
+      redirect_to contact_path
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_url
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :note, :contact_id, :profile_picture)
+  end
+      
 end
