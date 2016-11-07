e9498bfca2198579122a9640ee5224a2c09a956f
Ji You <kroyagis@gmail.com>
2016-11-07 16:35:19 -0500

    -redirects to the contact’s page after creation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to contacts_url, notice: 'The contact has been created.'
+      redirect_to contact_path(@contact), notice: 'The contact has been created.'
     else
       render :new
     end
