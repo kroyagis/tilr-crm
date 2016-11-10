e45e999cd73792db2ea249b9a2f32a817fff5786
Ji You <kroyagis@gmail.com>
2016-11-10 11:47:11 -0500

    -generated Group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107193015) do
+ActiveRecord::Schema.define(version: 20161110164458) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20161107193015) do
     t.integer  "contact_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
