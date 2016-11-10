1549b49775f0d18ca0852cc35a8a7b66a487354c
Ji You <kroyagis@gmail.com>
2016-11-10 11:50:03 -0500

    -associated groups and contacts by has_and_blongs_to_many

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
   validates_format_of :email, :with => /@/, :message => "Invalid email address"
 
+  has_and_blongs_to_many :groups
+
   def self.search(search)
     if search
       where("first_name like :s or last_name like :s or email like :s or first_name || ' ' || last_name || ' ' || email like :s", :s => "%#{search}%")
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
+
+  has_and_blongs_to_many :contacts
+
 end
