9d405c0d2cb30d11ce2df2ffa29b30f3f776ab70
Ji You <kroyagis@gmail.com>
2016-11-11 22:58:52 -0500

    -mounting image uploader

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,12 @@
 class Contact < ApplicationRecord
+  #mounting the iamge uploader to be used with the post model
+  mount_uploader :profile_picture, ProfilePictureUploader
+
   validates :first_name, :email, presence: true
   validates_format_of :email, :with => /@/, :message => "Invalid email address"
 
   has_and_belongs_to_many :groups
+  # accepts_nested_attributes_for :groups, allow_destroy: true, reject_if: :all_blank
 
   def self.search(search)
     if search
