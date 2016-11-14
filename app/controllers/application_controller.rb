9877ee879a1c8ab09c8afe3f4315300431e79326
Ji You <kroyagis@gmail.com>
2016-11-14 01:13:31 -0500

    -sanitize the params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def safe_params
+    params.except(:host, :port, :protocol).permit!
+  end
+
+  helper_method :safe_params
+
 end
